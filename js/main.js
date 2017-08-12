8e2252701a14e32d9b9c53ff2a68ed9fffb3c50b
Ahmed Talat <ahmedelsaeed105@gmail.com>
2017-08-12 03:41:34 +0300

    =menuandxs

--- js/main.js
+++ js/main.js
@@ -1,64 +1,73 @@
-let id = 1;
-$(document).ready(function(){
-	$("#1").fadeIn();
+// let id = 1;
+// $(document).ready(function(){
+// 	$("#1").fadeIn();
 	
-	 start();
-});
-function start(){
-	fading();
-	setInterval(function(){
-			 count();
+// 	 start();
+// });
+// function start(){
+// 	fading();
+// 	setInterval(function(){
+// 			 count();
 
-		next();
-		fading();
-		$("#"+ id).fadeIn()
-	},3000)
-}
-function next(){
-	id = id+1;
-}
+// 		next();
+// 		fading();
+// 		$("#"+ id).fadeIn()
+// 	},3000)
+// }
+// function next(){
+// 	id = id+1;
+// }
 
-function fading(){
-	$("img.xx").fadeOut();
-}
-function count(){
-	console.log($("div>img").length);
-	console.log(id);
-	if(id > $("div>img").length){
-		id=0;
+// function fading(){
+// 	$("img.xx").fadeOut();
+// }
+// function count(){
+// 	console.log($("div>img").length);
+// 	console.log(id);
+// 	if(id > $("div>img").length){
+// 		id=0;
 		
-	}
-}
+// 	}
+// }
 
-$(".arrowLeft").click(function(){
-	if(id <= 0){
-		id=0;
-		fading();
-		nex();
-		$("#"+id).fadeIn();
+// $(".arrowLeft").click(function(){
+// 	if(id <= 0){
+// 		id=0;
+// 		fading();
+// 		nex();
+// 		$("#"+id).fadeIn();
 
-	}
-	else{
-	id -=1;
-	fading();
-	$("#"+id).fadeIn();	
-	}
-});
-$(".arrowRight").click(function(){
-	if(id > $("div>img").length){
-		id=0;
-		fading();
-		next();
-		$("#"+id).fadeIn();
+// 	}
+// 	else{
+// 	id -=1;
+// 	fading();
+// 	$("#"+id).fadeIn();	
+// 	}
+// });
+// $(".arrowRight").click(function(){
+// 	if(id > $("div>img").length){
+// 		id=0;
+// 		fading();
+// 		next();
+// 		$("#"+id).fadeIn();
 
-	}
-	else{
-	next();
-	fading();
-	$("#"+id).fadeIn();	
-	}
+// 	}
+// 	else{
+// 	next();
+// 	fading();
+// 	$("#"+id).fadeIn();	
+// 	}
 
-});
+// });
 $("#options").click(function(){
-$(".Options").toggle();
+$(".fullMobile").toggle();
+$("#Header").toggle();
+$("#area").toggle();
+$("#third").toggle();
+});
+$("#close").click(function(){
+$(".fullMobile").hide();
+$("#Header").show();
+$("#area").show();
+$("#third").show();
 });
